0008d824038723a8bc29e84ead5badc1094ed7ab
MochRifkiAdriansyah <143516087+MochRifkiAdriansyah@users.noreply.github.com>
2023-10-26 21:12:33 +0700

    Update nilai.php

--- tugasswitchcase/nilai.php
+++ tugasswitchcase/nilai.php
@@ -1,54 +1,32 @@
 <?php
-// PHP Program to Find Grade of a Student using Switch Case
 
-// These are the marks of five subjects
-$nilai1 = 95;
-$nilai2 = 85;
-$nilai3 = 90;
-$nilai4 = 70;
-$nilai5 = 80;
 
-// It will calculate total, average, percentage, and grade
-$total = $nilai1 + $nilai2 + $nilai3 + $nilai4 + $nilai5;
-$average = $total / 5.0;
-$percentage = ($total / 500.0) * 100;
+$nilaimahasiswa = 100;
 
-// It will divides average by 10 check switch expression
-$sAvg = (int) ($average / 10);
-
-// It will calculate the Grade
-switch ($sAvg) {
-    case 10:
-        $grade = 'A';
-        break;
-
-    case 9:
-        $grade = 'A';
-        break;
-
-    case 8:
-        $grade = 'B';
-        break;
-
-    case 7:
-        $grade = 'C';
-        break;
-
-    case 6:
-        $grade = 'D';
-        break;
-
-    default:
-        $grade = 'E';
-        break;
+switch($nilaimahasiswa)
+{
+   case ($nilaimahasiswa > 90) :
+      $grade = "A";
+      break;
+   case ($nilaimahasiswa <= 90):
+      $grade = "B";
+      break;
+   case ($nilaimahasiswa <= 80):
+      $grade = "C";
+     
+      break;
+   case ($nilaimahasiswa <= 70):
+      $grade = "D";
+      break;
+   case ($nilaimahasiswa <= 60):
+      $grade = "E";
+     
+      break;
+  
+  
 }
 
-// It will print the final output
-echo "=========Hasil Nilai=========== <br>";
-echo "<br>";
-echo "Total Nilai           = " . $total . "/500\n<br>";
-echo "Nilai Rata-Rata       = " . $average . "\n <br>";
-echo "Nilai Presentation    = " . $percentage . "%\n<br>";
-echo "Grade                 = '" . $grade . "'\n<br>";
-
-?>
+echo "Hasil Studi Mahasiswa : " . "<br ?>";
+echo "Nilai Studi = " . $nilaimahasiswa;
+echo "<br />";
+echo "Nilai Grade = " . $grade;
