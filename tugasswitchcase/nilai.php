e92a73fc8f10fb7abdc60159943665420c37eff3
MochRifkiAdriansyah <143516087+MochRifkiAdriansyah@users.noreply.github.com>
2023-10-27 11:34:33 +0700

    Update nilai.php

--- tugasswitchcase/nilai.php
+++ tugasswitchcase/nilai.php
@@ -1,32 +1,56 @@
 <?php
 
+$nilaimhs = 91;
 
-$nilaimahasiswa = 100;
-
-switch($nilaimahasiswa)
+if ($nilaimhs >= 90 && $nilaimhs <=100)
 {
-   case ($nilaimahasiswa > 90) :
-      $grade = "A";
-      break;
-   case ($nilaimahasiswa <= 90):
-      $grade = "B";
-      break;
-   case ($nilaimahasiswa <= 80):
-      $grade = "C";
-     
-      break;
-   case ($nilaimahasiswa <= 70):
-      $grade = "D";
-      break;
-   case ($nilaimahasiswa <= 60):
-      $grade = "E";
-     
-      break;
-  
-  
+$grade = 'A';
 }
 
+elseif ($nilaimhs >= 80 && $nilaimhs <= 89)
+{
+$grade = 'B';
+}
+
+elseif ($nilaimhs >= 70 && $nilaimhs <= 79)
+{
+
+$grade = 'C';
+}
+
+elseif ($nilaimhs >= 60 && $nilaimhs <= 69)
+{
+$grade = 'D';
+}
+
+elseif ($nilaimhs >= 0 && $nilaimhs <= 59)
+{
+$grade = 'E';
+}
+echo"<br>";
 echo "Hasil Studi Mahasiswa : " . "<br ?>";
-echo "Nilai Studi = " . $nilaimahasiswa;
+echo "Nilai Studi = " . $nilaimhs;
 echo "<br />";
 echo "Nilai Grade = " . $grade;
+echo"<br>";
+switch ($grade)
+{
+case "A" :
+echo "Selamat anda mendapatkan nilai sempurna";
+break;
+case "B" :
+echo "Selamat nilai anda melebihi nilai rata-rata";
+break;
+case "C" :
+echo "Anda harus berusaha lebih keras lagi";
+break;
+case "D" :
+echo "Anda harus lebih berusaha lebih keras dan lebih rajin lagi";
+break;
+default :
+echo "Maaf anda kurang beruntung, coba ambil lagi di semester depan";
+break;
+}
+
+
+?>
